1d2dc75f58dcf258a1e53248dbbf14d1968d2899
Siyoung Hong <hongsizero@gmail.com>
2023-05-23 15:43:09 +0900

    GameEndScene Completed

--- src/scenes/GameEndScene.js
+++ src/scenes/GameEndScene.js
@@ -46,25 +46,44 @@ export default class GameEndScene extends Phaser.Scene {
         let rank = null;
         let rank_profile_bg = [];
         let rank_profile = [];
-        let profile_scale = 0.23;
+        let rank_nickname = [];
+        let profile_scale = 0.225;
+        let rank_wordstyle = {
+            font: "50px BR-R",
+            fill: "black",
+            align: 'center',
+            wordWrap: { width: 350, useAdvancedWrap: true },
+            lineSpacing: 10
+        };
         
         if(player_num == 2) { // 2 players
             rank = scene.add.image(960,1080,"rank_2p").setOrigin(0.5, 1);
+
             rank_profile_bg.push(scene.add.image(960-192,1080-693,"rank_first").setOrigin(0.5,0.5).setVisible(false));
             rank_profile_bg.push(scene.add.image(960+192,1080-537,"rank_second").setOrigin(0.5,0.5).setVisible(false));
+
             rank_profile.push(scene.add.image(960-192,1080-693-4,`${scene.playerRank[0].profile}`).setOrigin(0.5,0.5).setVisible(false).setScale(profile_scale));
             rank_profile.push(scene.add.image(960+192,1080-537-4,`${scene.playerRank[1].profile}`).setOrigin(0.5,0.5).setVisible(false).setScale(profile_scale));
+
+            rank_nickname.push(scene.add.text(960-192, 1080-693+223, `1등\n${this.playerRank[0].nickname}`, rank_wordstyle).setOrigin(0.5, 0.5).setVisible(false));
+            rank_nickname.push(scene.add.text(960+192, 1080-537+223, `2등\n${this.playerRank[1].nickname}`, rank_wordstyle).setOrigin(0.5, 0.5).setVisible(false));
+
         } else if (player_num == 1) {
             console.log("playernum = 1");
         } else { // 3 or 4 players
             rank = scene.add.image(960,1080,"rank_3p").setOrigin(0.5, 1);
+
             rank_profile_bg.push(scene.add.image(960,1080-691,"rank_first").setOrigin(0.5,0.5).setVisible(false));
             rank_profile_bg.push(scene.add.image(960-384,1080-535,"rank_second").setOrigin(0.5,0.5).setVisible(false));
             rank_profile_bg.push(scene.add.image(960+384,1080-472,"rank_third").setOrigin(0.5,0.5).setVisible(false));
+
             rank_profile.push(scene.add.image(960,1080-691-4,`${scene.playerRank[0].profile}`).setOrigin(0.5,0.5).setVisible(false).setScale(profile_scale));
             rank_profile.push(scene.add.image(960-384,1080-535-4,`${scene.playerRank[1].profile}`).setOrigin(0.5,0.5).setVisible(false).setScale(profile_scale));
             rank_profile.push(scene.add.image(960+384,1080-472-4,`${scene.playerRank[2].profile}`).setOrigin(0.5,0.5).setVisible(false).setScale(profile_scale));
-            console.log(`playernum: ${player_num}`);
+            
+            rank_nickname.push(scene.add.text(960, 1080-691+223, `1등\n${this.playerRank[0].nickname}`, rank_wordstyle).setOrigin(0.5, 0.5).setVisible(false));
+            rank_nickname.push(scene.add.text(960-384, 1080-535+223, `2등\n${this.playerRank[1].nickname}`, rank_wordstyle).setOrigin(0.5, 0.5).setVisible(false));
+            rank_nickname.push(scene.add.text(960+384, 1080-472+223, `3등\n${this.playerRank[2].nickname}`, rank_wordstyle).setOrigin(0.5, 0.5).setVisible(false));
         }
     
         scene.tweens.add({
@@ -81,11 +100,12 @@ export default class GameEndScene extends Phaser.Scene {
                       image.setVisible(true);
                     });
                     setTimeout(function () {
-                        rank_profile.forEach(image => {
-                            image.setVisible(true);
-                        });
-                      }, 200);
-                }, 200);
+                        for(let i=0; i<rank_profile.length; i++){
+                            rank_profile[i].setVisible(true);
+                            rank_nickname[i].setVisible(true);
+                        }
+                      }, 250);
+                }, 250);
                 setTimeout(function () {
         console.log("rank 애니메이션 완료");
       }, 1000);
